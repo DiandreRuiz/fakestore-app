647245e643ccacf099110dad94fb644e9725ecc6
DiandreRuiz <Diandreruiz77@gmail.com>
2025-09-11 17:24:04 -0400

    Finished bare minimum HomePage requirements

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 const HomePage = () => {
-    return <h1>HomePage</h1>;
+    return (
+        <Container>
+            <Row className="text-center mb-3">
+                <h1>Welcome to the Fake Store!</h1>
+                <h5>We have lots of exciting, fake items for you to see</h5>
+            </Row>
+            <Row className="justify-content-center">
+                <Col xs="auto">
+                    <Button as={Link} to="/products" variant="primary" size="sm">
+                        See Our Products
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default HomePage;
