ac6764f51d8e7d1eaa962097c309da7437438bb2
DiandreRuiz <Diandreruiz77@gmail.com>
2025-09-13 12:18:52 -0400

    Navbar and brand image updates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,14 @@ import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Brand from "../assets/react.svg";
+import Brand from "../assets/trendy_threads_logo.png";
 
 const NavBar = () => {
     return (
         <Navbar bg="light" variant="light" expand="md" className="p-3 mb-4">
             <Container>
                 <Navbar.Brand href="/" className="">
-                    <img src={Brand}></img>
+                    <img src={Brand} style={{width: "100px"}}></img>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" color="black" />
                 <Navbar.Collapse className="d-inline-block w-auto">
